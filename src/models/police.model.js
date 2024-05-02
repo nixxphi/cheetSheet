462111f9275d180bea7e391a528d59e8637c6552
nixxphi <123151475+nixxphi@users.noreply.github.com>
2024-05-02 11:36:16 +0100

    Create police.model.js

---
+++ src/models/police.model.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+// Define the schema for the police service
+const PoliceSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    contact_number: {
+        type: String,
+        required: true
+    },
+    availability: {
+        type: Boolean,
+        default: true 
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+    deleted: {
+        type: Boolean,
+        default: false
+    }
+});
+
+// Create a model for the police service
+const PoliceModel = mongoose.model('Police', PoliceSchema);
+
+export default PoliceModel;
